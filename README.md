7982263119813f9800f0909f340133bf236b9ce7
jfpa11100 <felipepala1000@gmail.com>
2025-05-08 14:31:12 -0600

    Get products from api

---
+++ README.md
@@ -0,0 +1,14 @@
+# Backend CIC
+
+## Development server
+To start a local development server, clone the repository, make sure you have python installed.
+
+first install fastApi runnig:
+```bash
+pip install "fastapi[standard]"
+```
+
+then run:
+```bash
+fastapi dev main.py
+```
--- main.py
+++ main.py
@@ -1,15 +1,52 @@
 from typing import Union
-
+from pydantic import BaseModel
 from fastapi import FastAPI
+from requests import get 
+from fastapi.middleware.cors import CORSMiddleware
+
+class Category(BaseModel):
+    id: int
+    name: str
+    slug: str
+    image: str
+    
+class Product(BaseModel):
+    id: int
+    title: str
+    slug: str
+    price: float
+    description: str
+    category: Category
 
 app = FastAPI()
 
+origins = [
+    "http://localhost.tiangolo.com",
+    "https://localhost.tiangolo.com",
+    "http://localhost",
+    # Default port when running angular
+    "http://localhost:4200",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+baseUrl = 'https://api.escuelajs.co/api/v1'
 
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
+    return {"Hello": "This is the api for the Test in the Creative Innovation Company. To get the products use /products "}
 
+@app.get("/products/")
+def get_products():
+    products = get(f"{baseUrl}/products")
+    return products.json()
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+@app.get("/products/{product_id}")
+def get_product_by_id(product_id: int, q: Union[str, None] = None):
+    return {"product_id": product_id, "q": q}
